b04b736f9163cdd15bc10d009aff2cb9acc8fd2e
cmmartin14 <corinna.m.martin@gmail.com>
2024-11-17 21:49:56 -0600

    displaying user profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/*
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,3 +9,18 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
